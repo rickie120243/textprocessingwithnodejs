58d898f0f730ffb7330fc6e4f703bdc16be8be6d
rickie120243 <rickie120243@gmail.com>
2015-08-11 13:31:00 +0800

    lesson11q3 done

--- lesson11q3.js
+++ lesson11q3.js
@@ -1,30 +1,17 @@
 var fs=require("fs");
 var list=fs.readFileSync("./lesson11q3-list.txt","utf8").split(/\r?\n/);
-var groupnumup=0;
-var groupnumdown=0;
-var numreplace=function(text){
-	return text.replace(/~(\d+)~/g,function(m,m1){
-		if(m1==="1") groupnumdown++;
-		return "~"+groupnumdown+"-"+m1+"~";
-	}); 
+var out_up=[],out_down=[];
+var onSort=function(line,num){
+	return line.replace(/~(\d+)~/g,function(m,m1){
+		if(m1==="1") num++;
+		return "~"+num+"-"+m1+"~";
+	});
 }
 
-/*var group_down=function(lines){
-	lines=lines.split(/\r?\n/).map(function(line){
-		return numreplace(line);
-	}).join("\n");
-	console.log(lines);
-} */
-var group_up=function(lines){
-	lines=lines.replace(/\r?\n/g,"");
-	lines=numreplace(lines);
-	console.log(lines); 
-}
-
+var groupnum=[0,0];
 var readFile=function(fn){
 	var content=fs.readFileSync("./"+fn,"utf8").split(/\r?\n/);
-	var arr_up=[];
-	var arr_down=[];
+	var arr_up=[],arr_down=[];
 	content.map(function(line){
 		if(line.match(/^~/g)){
 			arr_down.push(line);
@@ -32,7 +19,9 @@ var readFile=function(fn){
 			arr_up.push(line);
 		}
 	});
-//	arr_down.map(group_down);	
-	arr_up.map(group_up);
+	out_up.push(onSort(arr_up.join("\n"),groupnum[0]++));
+	out_down.push(onSort(arr_down.join("\n"),groupnum[1]++));	
 }
 list.map(readFile);
+fs.writeFileSync("up.txt",out_up.join("\n"),"utf8");
+fs.writeFileSync("down.txt",out_down.join("\n"),"utf8");
